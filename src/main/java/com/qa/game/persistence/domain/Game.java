068ad2837f9020f92384de5e7abdc4782dcf4299
Shaker Abdul <AShaker@academytrainee.com>
2020-10-15 15:07:07 +0100

    added Game.java

---
+++ src/main/java/com/qa/game/persistence/domain/Game.java
@@ -0,0 +1,60 @@
+package com.qa.game.persistence.domain;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Game {
+	private long id;
+	private String name;
+	private String genre;
+	private int price;
+
+	public Game() {
+		super();
+	}
+
+	public Game(String name, String genre, int price) {
+		super();
+		this.name = name;
+		this.genre = genre;
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Game [name=" + name + ", genre=" + genre + ", price=" + price + "]";
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+}
